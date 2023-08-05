3341393a80a6a08ca818199615e55c769bace797
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-05 13:17:47 -0400

    Added append and pop methods to DoublyLinkedList class

--- Doubly_linked_lists/DoublyLinkedList.py
+++ Doubly_linked_lists/DoublyLinkedList.py
@@ -16,3 +16,36 @@ class DoublyLinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        length += 1
+        return True
+    
+    def pop(self):
+        if self.head == None:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.tail = None
+            self.head = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        length -= 1
+        return temp
+            
+            
+            
+            
+my_doubly_linked_list = DoublyLinkedList(7)
+
+my_doubly_linked_list.print_list()
