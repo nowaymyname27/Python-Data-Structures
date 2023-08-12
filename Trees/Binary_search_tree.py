d43febdd082ad9d8a125fa579e3e3d74388a7529
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-12 10:17:54 -0400

    Added constructo to BST class

---
+++ Trees/Binary_search_tree.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+
+class Binary_Search_Tree:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.root = new_node
