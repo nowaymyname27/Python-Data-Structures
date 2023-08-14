c742a507ed334140d256878b83637c91ebf39c6f
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-14 18:59:52 -0400

    Added an insert method

--- Trees/Binary_search_tree.py
+++ Trees/Binary_search_tree.py
@@ -6,8 +6,36 @@ class Node:
         
 
 class Binary_Search_Tree:
-    def __init__(self, value):
-        new_node = Node(value)
-        self.root = new_node
+    def __init__(self):
+        self.root = None
     
     def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+            
+my_BST = Binary_Search_Tree()
+
+my_BST.insert(2)
+my_BST.insert(1)
+my_BST.insert(3)
+
+print(my_BST.root.value)
+print(my_BST.root.left.value)
+print(my_BST.root.right.value)
+        
