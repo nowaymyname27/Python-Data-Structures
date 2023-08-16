b92262563dcf0043682e3f9a137bc7025466f6a6
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-16 19:21:10 -0400

    Adding a keys method

--- Hash Tables/Hash_table.py
+++ Hash Tables/Hash_table.py
@@ -25,6 +25,14 @@ class HashTable:
                 if self.data_map[index][i][0] == key:
                     return self.data_map[index][i][1]
         return None
+    
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
         
     
 my_hash_table = HashTable()
@@ -33,5 +41,4 @@ my_hash_table.set_item("nails", 100)
 my_hash_table.set_item("tile", 50)
 my_hash_table.set_item("lumber", 80)
 
-print(my_hash_table.get_item("lumber"))
-print(my_hash_table.get_item("bolts"))
+print(my_hash_table.keys())
