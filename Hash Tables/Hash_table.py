8c46956b8370d702d3a8a5c6599767025c8113b5
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-16 19:08:33 -0400

    Fixing a small error in the get_item method

--- Hash Tables/Hash_table.py
+++ Hash Tables/Hash_table.py
@@ -20,7 +20,7 @@ class HashTable:
         
     def get_item(self, key):
         index = self.__hash(key)
-        while self.data_map[index] is not None:
+        if self.data_map[index] is not None:
             for i in range(len(self.data_map[index])):
                 if self.data_map[index][i][0] == key:
                     return self.data_map[index][i][1]
