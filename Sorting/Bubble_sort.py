496503676f6e793435d72bef3602a5b7f79eeb0a
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-22 19:49:59 -0400

    Adding a sorting folder, starting with Bubble_sort

---
+++ Sorting/Bubble_sort.py
@@ -0,0 +1,14 @@
+def bubble_sort(my_list):
+    for i in range(len(my_list) - 1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j + 1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = temp
+    return my_list
+                
+        
+        
+my_list = [4,3,2,1]
+
+print(bubble_sort([4,3,2,1]))
