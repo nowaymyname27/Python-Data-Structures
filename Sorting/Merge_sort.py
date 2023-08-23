eb0bf9e099757cc19344ffaf545bca7aa1e18456
Jose Ramirez <rosejamirez27@gmail.com>
2023-08-23 16:21:33 -0400

    Debugging helper function

--- Sorting/Merge_sort.py
+++ Sorting/Merge_sort.py
@@ -3,17 +3,19 @@ def merge(list1, list2):
     i = 0
     j = 0
     while i < len(list1) and j < len(list2):
-        if list1[i] < list2[i]:
+        if list1[i] < list2[j]:
             combined.append(list1[i])
             i += 1
         else:
             combined.append(list2[j])
             j += 1
     while i < len(list1):
-        combined.append(i)
+        combined.append(list1[i])
         i += 1
     while j < len(list2):
-        combined.append(j)
+        combined.append(list2[j])
         j += 1
     
     return combined
+
+print(merge([1,5,7,9], [2,3,4,6,8]))
